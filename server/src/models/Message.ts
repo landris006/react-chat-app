8e984af336a20df13d525651658334a693657f9c
landris006 <laczko.andris007@gmail.com>
2022-06-13 18:32:57 +0200

    updated Message schema

--- server/src/models/Message.ts
+++ server/src/models/Message.ts
@@ -2,8 +2,9 @@ import mongoose, { Model } from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  from: { type: String, required: true },
-  to: { type: String, required: true },
+  senderId: { type: String, required: true },
+  senderUsername: { type: String, required: true },
+  receiverId: { type: String, required: true },
   createdAt: { type: String, default: Date() },
 });
 
--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -6,8 +6,9 @@ type eventHandler = (
 interface Message {
   _id: string;
   content: string;
-  from: string;
-  to: string;
+  senderId: string;
+  senderUsername: string;
+  receiverId: string;
   createdAt: string;
 }
 
